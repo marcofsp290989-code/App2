a6eb6df724ba9c2503c0b16000df38824ecc8ac0
marcofsp290989-code <marcofsp290989@gmail.com>
2025-11-21 00:27:37 +0000

    Upgrade Gradle plugins and dependencies
    Updated Kotlin and Android Gradle plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,27 @@
-plugins { kotlin("jvm") version "1.8.21" apply false }
-allprojects { repositories { google(); mavenCentral() } }
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
