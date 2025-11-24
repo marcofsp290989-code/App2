298c3905522269f3ac35823132320584bb1bae1d
marcofsp290989-code <marcofsp290989@gmail.com>
2025-11-24 18:26:57 +0000

    Delete app directory

--- app/src/main/java/com/adaptive/power/profile/ProfileDetector.kt
+++
@@ -1,7 +0,0 @@
-
-package com.adaptive.power.profile
-import android.content.Context
-class ProfileDetector(private val ctx: Context) {
-    enum class Profile { IDLE, LIGHT, WEB, VIDEO, GAMING, HEAVY }
-    fun detectNow(): Profile = Profile.IDLE
-}
