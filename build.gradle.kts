ec9a9fdcf5f863b7449421b09e13369e12ed3318
marcofsp290989-code <marcofsp290989@gmail.com>
2025-12-10 18:48:46 +0000

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-
-plugins {
-    kotlin("jvm") version "1.8.0" apply false
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-}
